ca5b45082489e5fc21a44cce273950333324dafa
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-09-12 21:03:30 +0100

    Update __init__.py and setup.py
    Added more metadata

--- cmpx/__init__.py
+++ cmpx/__init__.py
@@ -1,5 +1,11 @@
 from . import Complex
 
+__title__      = 'PythonComplex'
+__author__     = 'Omar Belghaouti'
+__maintainer__ = 'Omar Belghaouti'
+__email__      = 'bel_omar18@yahoo.com'
+__version__    = '0.5'
+__license__    = 'MIT'
 __all__ = [
     'Complex'
 ]
--- setup.py
+++ setup.py
@@ -5,17 +5,24 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='cmpx',
-    version='0.3',
+    version='0.4',
     description='Complex class for different operations on complex numbers',
     long_description_content_type='text/markdown',
     long_description=README,
     license='MIT',
     packages=find_packages(),
     author='Omar Belghaouti',
+    maintainer='Omar Belghaouti',
     author_email='bel_omar18@yahoo.com',
+    maintainer_email='bel_omar18@yahoo.com',
     keywords=['Complex', 'ComplexOperations', 'ComplexNumbers'],
     url='https://github.com/Omar-Belghaouti/PythonComplex',
-    download_url='https://pypi.org/project/cmpx/'
+    download_url='https://pypi.org/project/cmpx/',
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ]
 )
 
 if __name__ == '__main__':
