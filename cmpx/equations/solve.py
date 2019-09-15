547084d53cda416ce2b5b64101875ab01155ab54
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-09-15 19:58:25 +0100

    Bug fix

--- cmpx/equations/solve.py
+++ cmpx/equations/solve.py
@@ -2,13 +2,19 @@
 from cmpx import Complex
 from cmpx import print_err
 from math import sqrt
-
-# Function to solve a second degree (or linear) equation with passing arguments seperatly
+"""
+Function to solve a second degree (or linear) equation with passing arguments seperatly
+eq = a * x**2 + b * x + c
+"""
 def solve(a, b, c=0):
     try:
-        if a or a and b == 0 :
+        if a and b == 0:
             raise ValueError('At least give a number to the first two coefficents')
-        if c != 0:
+        if a and b != 0:
+            if a == 0:
+                return (Complex(-c/b))
+            if b == 0:
+                return (Complex(sqrt(-c/a))) if -c/a >= 0 else (Complex(0, sqrt(c/a)))
             delta = b**2 - 4 * a * c
             if(delta < 0):
                 solution = (Complex(-b/2*a, -sqrt(-delta)/2*a),
@@ -19,7 +25,5 @@ def solve(a, b, c=0):
                 solution = (Complex((-b - sqrt(delta))/2*a),
                             Complex((-b + sqrt(delta))/2*a))
             return solution
-        else:
-            return (Complex(-b/a))
     except (ZeroDivisionError, ValueError) as err:
         print_err(err)
