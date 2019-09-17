b8573637ec804e5f7b00eaaa9d5d0a51ad974f55
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-09-17 16:02:29 +0100

    Updating tutorial+Updation version to 0.8.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='cmpx',
-    version='0.8.3',
+    version='0.8.4',
     description='A package for different operations on complex numbers',
     long_description_content_type='text/markdown',
     long_description=README,
