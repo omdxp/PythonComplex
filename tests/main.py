f44ddfc4312c336c945349db392ac240ad6e414c
Omar-Belghaouti <bel_omar18@yahoo.com>
2019-09-17 15:04:49 +0100

    Create Jupyter notebook tutorial

--- tests/main.py
+++ tests/main.py
@@ -4,13 +4,8 @@ from cmpx.equations import solve
 def main():
     arr = []
     for i in range(10):
-        print(i)
         arr.append(Complex(i, 2*i))
     
-    for i in range(1,10):
-        print(arr[i])
-    
-    a = Complex()
-    print(a)
+    print(arr)
 
 if __name__ == '__main__': main()
